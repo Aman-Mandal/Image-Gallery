20d4ee2716a469478ce5321f85bcdc1590303b94
Aman-Mandal <002amankumar@gmail.com>
2022-06-30 18:26:17 +0530

    loading modal

--- src/App.js
+++ src/App.js
@@ -1,31 +1,33 @@
-import { useEffect, useState } from 'react'
-import ImageCard from './Components/UI/ImageCard'
+import { useEffect, useState } from "react";
+import Loading from "./Components/Modal/Loading";
+import ImageCard from "./Components/UI/ImageCard";
 
 const App = () => {
-  const [images, setImages] = useState([])
-  const [isLoading, setIsLoading] = useState(true)
-  const [term, setTerm] = useState('cat')
+  const [images, setImages] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [term, setTerm] = useState("cat");
 
   useEffect(() => {
     fetch(
       `https://pixabay.com/api/?key=${process.env.REACT_APP_PIXABAY_API_KEY}&q=${term}&image_type=photo`
     )
-      .then(res => res.json())
-      .then(data => {
-        console.log(data)
-        setImages(data.hits)
-        setIsLoading(false)
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        setImages(data.hits);
+        setIsLoading(false);
       })
-      .catch(err => console.log(err))
-  }, [])
+      .catch((err) => console.log(err));
+  }, []);
+
   return (
     <div className="container mx-auto my-7">
       <div className="grid grid-cols-3 gap-6 md:">
-        {images.map(image => (
+        {images.map((image) => (
           <ImageCard key={image.id} image={image} />
         ))}
       </div>
     </div>
-  )
-}
-export default App
+  );
+};
+export default App;
